bdc4a2de28203250e6c76540a7668f2153bfb540
Vakili <p.vakili@avajang.com>
2022-04-25 16:49:47 +0430

    test-quasar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -73,35 +73,7 @@
               </q-item>
             </q-list>
           </q-expansion-item>
-          <q-expansion-item
-            icon="fa fa-gear"
-            class="text-subtitle2"
-            :label="$t('SystemManagement')"
-          >
-            <q-list class="q-pl-lg"> </q-list>
-            <q-list class="q-pl-lg">
-              <q-item to="/titles" active-class="q-item-no-link-highlighting">
-                <q-item-section avatar>
-                  <q-icon name="fa fa-file-lines" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label class="text-subtitle2">{{
-                    $t("Titles")
-                  }}</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item to="/userManagement" active-class="q-item-no-link-highlighting">
-                <q-item-section avatar>
-                  <q-icon name="fa fa-person" />
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label class="text-subtitle2">{{
-                    $t("UserManagement")
-                  }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-expansion-item>
+       <SystemManagement/>
           
         </q-list>
       </q-drawer>
@@ -121,7 +93,8 @@ import { useI18n } from "vue-i18n";
 
 export default {
   components:{
-   HakupianButtonDropdown: defineAsyncComponent(() =>import("./HakupianButtonDropdown.vue"))
+   HakupianButtonDropdown: defineAsyncComponent(() =>import("./HakupianButtonDropdown.vue")),
+   SystemManagement:defineAsyncComponent(() =>import('./SystemManagement.vue'))
   },
   setup () {
         const { t } = useI18n();
---
+++ src/layouts/SystemManagement.vue
@@ -0,0 +1,41 @@
+<template>
+     <q-expansion-item
+            icon="fa fa-gear"
+            class="text-subtitle2"
+            :label="$t('SystemManagement')"
+          >
+            <q-list class="q-pl-lg"> </q-list>
+            <q-list class="q-pl-lg">
+              <q-item to="/titles" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="fa fa-file-lines" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-subtitle2">{{
+                    $t("Titles")
+                  }}</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item to="/userManagement" active-class="q-item-no-link-highlighting">
+                <q-item-section avatar>
+                  <q-icon name="fa fa-person" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-subtitle2">{{
+                    $t("UserManagement")
+                  }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-expansion-item>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
